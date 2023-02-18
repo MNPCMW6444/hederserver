e3d1bc6f61eae4d24ba3b55c78650b28f9d998b0
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-02-18 13:44:26 +0200

    Update mmmRouter.js

--- built/routers/mmmRouter.js
+++ built/routers/mmmRouter.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mmm_1 = __importDefault(require("../models/mmm"));
-const nnn_1 = __importDefault(require("../models/nnn"));
 const router = express_1.default.Router();
 router.post("/addnew", async (req, res) => {
     try {
@@ -36,24 +35,5 @@ router.get("/getall", async (req, res) => {
             .status(500)
             .json({ serverError: "Unexpected error occurred in the server" });
     }
-});
-router.get("/michael", async (req, res) => {
-    try {
-        const aaaaa = await nnn_1.default.find();
-        res.json({ yoad: aaaaa[0].name === "open", dsfds: "gfssdf" });
-    }
-    catch (err) {
-        console.error(err);
-        res.json({ yoad: false, dsfds: "gfssdf" });
-    }
-});
-router.post("/open", async (req, res) => {
-    setTimeout(() => nnn_1.default.remove({}, function (err) {
-        console.log("collection removed");
-    }), 120000);
-    const what = req.body.what;
-    const s = new nnn_1.default({ name: what });
-    await s.save();
-    res.json({ yoad: what, dsfds: "gfssdf" });
 });
 exports.default = router;
