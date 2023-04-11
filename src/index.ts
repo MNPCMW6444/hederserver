c9333fdd2d03aa4cb04490611e98e13d6b4d455c
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-04-11 11:53:04 +0300

    awd

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import sslRedirect from "heroku-ssl-redirect";
-import express, { Request } from "express";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose, { ConnectOptions } from "mongoose";
@@ -8,7 +8,6 @@ import userRouter from "./routers/userRouter";
 import cookieParser from "cookie-parser";
 import memoryRouter from "./routers/memoryRouter";
 import responseRouter from "./routers/responseRouter";
-import winston from "winston";
 import "winston-mongodb";
 
 const app = express();
@@ -17,13 +16,10 @@ const port = process.env.PORT || 6555;
 dotenv.config();
 
 let mainDbStatus = false;
-let oCDbStatus = false;
-
-let logReq: any;
 
 const connectToDBs = () => {
   mongoose.connect(
-    "" + process.env.MONGO,
+    "" + process.env.SAFE,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -34,52 +30,8 @@ const connectToDBs = () => {
       mainDbStatus = true;
     }
   );
-  try {
-    const mongoTransport = winston.add(
-      new winston.transports.MongoDB({
-        db: "" + process.env.MONGO_OC,
-        options: {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        },
-      })
-    );
 
-    const logger = winston.createLogger({
-      level: "info",
-      format: winston.format.json(),
-      defaultMeta: { service: "user-service" },
-      transports: [mongoTransport],
-    });
-
-    logReq = (req: Request<{}, any, any, Record<string, any>>) =>
-      logger.log({
-        level: "warn",
-        message:
-          "Req: " +
-          JSON.stringify({
-            headers: req.headers,
-            method: req.method,
-            url: req.url,
-            httpVersion: req.httpVersion,
-            body: req.body,
-            cookies: req.cookies,
-            path: req.path,
-            protocol: req.protocol,
-            query: req.query,
-            hostname: req.hostname,
-            ip: req.ip,
-            originalUrl: req.originalUrl,
-            params: req.params,
-          }),
-      });
-
-    oCDbStatus = true;
-  } catch (err) {
-    console.log(err);
-    oCDbStatus = false;
-  }
-  if (!mainDbStatus || !oCDbStatus) setTimeout(connectToDBs, 180000);
+  if (!mainDbStatus) setTimeout(connectToDBs, 180000);
 };
 
 connectToDBs();
@@ -92,21 +44,6 @@ app.use(cors());
 app.use(cookieParser());
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
-
-app.use((req, res, next) => {
-  if (mainDbStatus || oCDbStatus) {
-    logReq
-      ? logReq(req)
-      : console.log(
-          "didnt log but shoud have because it is: ",
-          JSON.stringify(logReq)
-        );
-    next();
-  } else
-    res
-      .status(500)
-      .json({ serverError: "Server is down now. Please try again later." });
-});
 
 app.use("/mmm", mmmRouter);
 app.use("/user", userRouter);
